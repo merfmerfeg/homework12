a87734a801a9119c07f0d935da678675885ef4da
Roman Mai <merfeg@yandex.ru>
2019-10-14 23:12:23 +0800

    new

--- routes/cards.js
+++
@@ -1,8 +0,0 @@
-const cardsRoute = require('express').Router();
-const cardsData = require('../data/cards.json');
-
-cardsRoute.get('/cards', (req, res) => {
-  res.send(cardsData);
-});
-
-module.exports = cardsRoute;
